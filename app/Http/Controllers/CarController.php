d85cab0797c693085026440a92624809463202f0
johnjoel061 <alfabetejohnjoel@gmail.com>
2025-03-14 15:54:50 +0800

    ...

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -6,8 +6,13 @@ use Illuminate\Http\Request;
 
 class CarController extends Controller
 {
+   
     public function index()
     {
         return 'Index method from CarController';
+    }
+    public function __invoke()
+    {
+        return '__invoke';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CarController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,6 +9,12 @@ Route::get('/', function () {
 });  
 
 //Route::view('/about', 'about')->name('about');
-Route::controller(CarController::class)->group(function(){
-    Route::get('/car', 'index');
-});
+// Route::controller(CarController::class)->group(function(){
+//     Route::get('/car', 'index');
+//     Route::get('/my-cars','myCars');
+// });
+
+Route::get('/car', [CarController::class, 'index']);
+Route::get('/car/invokable', CarController::class);
+
+Route::resource('/products', ProductController::class)->except(['destroy']);

8461873d9cb342356711363a5d4e15613865cfea
johnjoel061 <alfabetejohnjoel@gmail.com>
2025-03-14 13:42:11 +0800

    ...

---
+++ resources/views/about.blade.php
@@ -0,0 +1,3 @@
+<h1>Hello Welcome to my LARAVEL course</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi magnam cupiditate aut. Provident facere minus ut?
+    Sit maxime voluptatem suscipit, laborum et recusandae, dolorum consequatur obcaecati non iste debitis numquam?</p>
